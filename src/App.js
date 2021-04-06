a1499519948b5a34221cca688489030d2989c47a
pyuser <enes.karakas.e@gmail.com>
2021-04-06 09:39:26 +0300

    finished style lesson 71 done.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import HomePage from "./pages/homepage/Homepage"
+import "./App"
 
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <HomePage />
-      </div>
+        <div className="App">
+          <HomePage />
+        </div>
     )
   }
 }
--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,35 +1,11 @@
 import React from 'react';
+import Directory from '../../components/directory/Directory';
 import "./homepage.styles.scss";
 
 export default function Homepage() {
     return (
         <div className="homepage">
-            <div className="directory-menu">
-                
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">TITLE</h1>
-                        <span className="content">SUBTITLE</span>
-                    </div>
-                </div>
-
-
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">TITLE 2</h1>
-                        <span className="content">SUBTITLE</span>
-                    </div>
-                </div>
-
-
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">TITLE</h1>
-                        <span className="content">SUBTITLE</span>
-                    </div>
-                </div>
-                
-            </div>
+            <Directory />
         </div>
     )
 }
